7a30bb45d0f5883ccb3ea8f9144dff45c1ee68c7
sorawitr0607 <sorawit.r@tgh.co.th>
2024-10-07 13:20:59 +0700

    Update streamlit_demo.py

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 @st.cache_data #ทำให้ไม่ต้องโหลด dataset ใหม่ทุกรอบ
 def load_data():
-    df_agg = pd.read_csv(r'C:\Users\Sorawitr\Desktop\streamlit\ken_jee _youtube_dataset\Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+    df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                           'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
                           'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
@@ -17,9 +17,9 @@ def load_data():
     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
     df_agg.sort_values('Video publish time', ascending = False, inplace = True)
-    df_agg_sub = pd.read_csv(r'C:\Users\Sorawitr\Desktop\streamlit\ken_jee _youtube_dataset\Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-    df_comment = pd.read_csv(r'C:\Users\Sorawitr\Desktop\streamlit\ken_jee _youtube_dataset\All_Comments_Final.csv')
-    df_time = pd.read_csv(r'C:\Users\Sorawitr\Desktop\streamlit\ken_jee _youtube_dataset\Video_Performance_Over_Time.csv')
+    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_comment = pd.read_csv('All_Comments_Final.csv')
+    df_time = pd.read_csv('Video_Performance_Over_Time.csv')
     return df_agg,df_agg_sub,df_comment,df_time
 
 #Create dataframes
